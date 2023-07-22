8e38f137524340ca14c21d0c34a1680536b5bf55
Jillian Fussner <jillian.fussner@gmail.com>
2023-07-22 17:41:55 -0500

    All tests passing

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,9 +1,38 @@
-const Buttons = () => {
+// import React from "react";
+// import satData from "./satData";
+
+// const Buttons = ({filterByType, setSat, displaySats}) => {
+
+// return (
+//   <div>
+//     {displaySats.map((sat, index) => (
+//       <button onClick={() => filterByType(sat)} key={index}>
+//         {satType} Orbit
+//       </button>
+//     ))}
+//     <button onClick={() => setSat(satData)}>
+//       All Orbits
+//     </button>
+//   </div>
+// );
+// };
+
+
+// export default Buttons;
+
+import React from "react";
+import satData from "./satData";
+
+const Buttons = ({ filterByType, setSat, displaySats }) => {
   return (
     <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
-      </div>
+      {displaySats.map((satType, index) => (
+        <button onClick={() => filterByType(satType)} key={index}>
+          {satType} Orbit
+        </button>
+      ))}
+      <button onClick={() => setSat(satData)}>All Orbits</button>
+    </div>
   );
 };
 
